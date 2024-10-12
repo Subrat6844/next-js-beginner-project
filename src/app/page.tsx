b2be4bd0ab6847834d7c06d452d6216d71b106c2
Subrat <chaudharysubrat78@outlook.com>
2024-10-12 23:25:50 +0530

    added webinars section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,8 @@
 import FeasuredCardSection from "@/components/FeasuredCardSection";
 import HeroSection from "@/components/HeroSection";
+import Instructors from "@/components/Instructors";
 import TestimonialCards from "@/components/TestimonialCards";
+import UpcomingWebinars from "@/components/UpcomingWebinars";
 import WhyChooseUs from "@/components/WhyChooseUs";
 
 export default function Home() {
@@ -10,6 +12,8 @@ export default function Home() {
 			<FeasuredCardSection />
 			<WhyChooseUs />
 			<TestimonialCards/>
+			<UpcomingWebinars/>
+			<Instructors/>
 		</main>
 	);
 }
