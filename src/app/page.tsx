9cc3a12d638dfe274ecfad7c4f3bb312915b0b1f
Subrat <chaudharysubrat78@outlook.com>
2024-10-12 12:54:40 +0530

    added bg gradient and sticky scroll

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,15 @@
+import FeasuredCardSection from "@/components/FeasuredCardSection";
 import HeroSection from "@/components/HeroSection";
+import TestimonialCards from "@/components/TestimonialCards";
+import WhyChooseUs from "@/components/WhyChooseUs";
 
 export default function Home() {
-  return (
-   <main className="min-h-screen bg-black/[0.96] bg-grid-white/[0.06] antialiased">
-    <HeroSection/>
-   </main>
-  );
+	return (
+		<main className="min-h-screen bg-black/[0.96] bg-grid-white/[0.06] antialiased">
+			<HeroSection />
+			<FeasuredCardSection />
+			<WhyChooseUs />
+			<TestimonialCards/>
+		</main>
+	);
 }
